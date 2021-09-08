aadc03a6cd9fb2a3ef35dc0ddc0171c0ecbf462f
Monkiky <monkiky@Monkikys-Mac-mini.local>
2021-09-08 11:09:52 +0100

    Testing

--- Testing/test_vcf-matcher.py
+++ Testing/test_vcf-matcher.py
@@ -1,1 +1,11 @@
-s
+
+
+'''
+This script breaks down fragments of the file run.py 
+to carry out different tests
+'''
+import pytest
+
+
+# First test: Check if all row in the body of the vcf files are taken 
+

2a6ebecfd8d662bd08d85110e21470e600acdc82
Monkiky <monkiky@Monkikys-MacBook-Pro.local>
2021-09-15 09:35:29 +0100

    Improving code

--- app/run.py
+++ app/run.py
@@ -14,198 +14,187 @@ import numpy as np
 
 # To allow the user to introduce the file after running "python run.ppy"
 
-# Example:    python run.py -- /path/file_name1 -- /path/file_name2
+# Example:    python run.py --vcf1 /path/file_name1 --vcf2 /path/file_name2
 
    ### 1. Load the files
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--vcf1', type=str, required=True)
-parser.add_argument('--vcf2', type=str, required=True)
-args = parser.parse_args()
-NAME_FILE_1 = args.vcf1
-NAME_FILE_2 = args.vcf2
+# Take only the data contains in the body of the vcf files.
 
+def load_sample (Name_file):
+    '''
+    Take the header of the body of the CSV file
+    '''
+    with open(Name_file, 'r') as f:
+        for line in f:
+            if line.startswith('#') and len(line)>2 and line[1] != '#':
+                columns = line[1:-1].split('\t')
+                data = pd.read_csv(Name_file, comment='#', delimiter='\t', names=columns)
+                break
+    return data
 
-   ### 2. Take only the data contains in the body of the vcf files.
+    ### 2. One or > one sample in the files? 
 
-# Originally what I did was to delete the metedata and save in a new file the data
-# This approach was over complicated and doest´t work in window machines.
-# Instead of this, the new approach directly take the data and save it in pandas dataframe object.
-# This is also a more efficient method.
-
-# For the file number 1
-with open(NAME_FILE_1, 'r') as f:
-    for line in f:
-        if line.startswith('#') and len(line)>2 and line[1] != '#':
-            columns = line[1:-1].split('\t')
-            break
-
-dataA = pd.read_csv(NAME_FILE_1, comment='#', delimiter='\t', names=columns)
-
-# For the second file
-with open(NAME_FILE_2, 'r') as f:
-    for line in f:
-        if line.startswith('#') and len(line)>2 and line[1] != '#':
-            columns = line[1:-1].split('\t')
-            break
-dataB = pd.read_csv(NAME_FILE_2, comment='#', delimiter='\t', names=columns)
-
-
-# If the pd data frame has more than 10 columns
-# that means there are more than 1 sample in the vcf file
-# If more than one file is found, next lines ask which sample to take.
-
-if len(dataA.columns) > 10:
-    Name_sample1 = input('It seems that {} has more than one sample. Please introduce the name of the sample you wish to analyse:  '.format(os.path.basename(NAME_FILE_1)))
-else:
-    print('File', os.path.basename(NAME_FILE_1), 'contains one sample only.')
-    Name_sample1 = dataA.columns.tolist()[9]
-
-if len(dataB.columns) > 10:
-    Name_sample2 = input('It seems that {} has more than one sample. Please introduce the name of the sample you wish to analyse:  '.format(os.path.basename(NAME_FILE_2)))
-else:
-    print('File', os.path.basename(NAME_FILE_2), 'contains one sample only.')
-    Name_sample2 = dataB.columns.tolist()[9]
-
+def take_sample(data,NAME_FILE):
+    '''
+    VCF files can have 1 or > 1 samples
+    We need to know what sample the user wants to compare
+    After this, the data of the sample is all stored in one  column
+    to access the data, it needs to  split it in different columns
+    '''
+    if len(data.columns) > 10:
+        Name_sample = input('It seems that {} has more than one sample. Please introduce the name of the sample you wish to analyse:  '.format(os.path.basename(NAME_FILE)))
+    else:
+        print('File', os.path.basename(NAME_FILE), 'contains one sample only.')
+        Name_sample = data.columns.tolist()[9]
+    
+    tmp_df_2 = data[Name_sample].str.split(':', expand=True)
+    # I have seen very unlikely Na values
+    # To solve this, they are deleted
+    tmp_df_2 = tmp_df_2.fillna(value=np.nan)
+    tmp_df_2 = tmp_df_2.dropna(axis=1, how='any')
+    # To get the name of the new columns created
+    tmp_df_2.columns = data.FORMAT.iloc[0].split(':')
+    return pd.concat([data, tmp_df_2], axis=1),Name_sample
+    
     ### 3. Filter the variants
 
-# Two filters are applied. First, I take only the variants that "PASS" the "FILTER" field.
-# This is a mandatory field.
+def filter_1(data):
+    '''
+    Take only the variants that "PASS" the "FILTER" field
+    '''
+    return data.query('FILTER == "PASS"')
 
-# Then, I take only the variants which Variant Frequency (VF) is highter or equal than. 0.4
-# This is not a mandatory field in vcf filed
 
-# VF is in the 9th column with another data. Example: 0/1:100:3501,254:0.0676:20:-100.0000:100
-# we need to extract and put VF data in an independent column first
-df2 = dataA[Name_sample1].str.split(':', expand=True)
-
-df2 = df2.fillna(value=np.nan)
-df2 = df2.dropna(axis=1, how='any')
-
-df2.columns = dataA.FORMAT.iloc[0].split(':')
-dataA = pd.concat([dataA, df2], axis=1)
-
-# Same for dataB
-df3 = dataB[Name_sample2].str.split(':', expand=True)
-df3 = df3.fillna(value=np.nan)
-df3 = df3.dropna(axis=1, how='any')
-
-df3.columns = dataB.FORMAT.iloc[0].split(':')
-dataB = pd.concat([dataB, df3], axis=1)
-
-# Now, let´s filter the variant
-# We don't want variants that doesn't pass the Filter status
-# This field is mandatory, no condition is needed because it is applied to any type of VCF file.
-dataA = dataA.query('FILTER == "PASS"')
-dataB = dataB.query('FILTER == "PASS"')
-
-# Now, I apply Variant Frequency (VF) filter (non-mandatory field)
-
-# If there is a VF Genotype fields in the file
-# Take the variants that are > 0.4
-# If not, no VF filter is applied
-
-if "VF" in dataA.columns:
-    dataA["VF"] = dataA["VF"].astype(float)
-    dataA = dataA.query('VF > 0.4')
-
-if "VF" in dataB.columns:
-    dataB["VF"] = dataB["VF"].astype(float)
-    dataB = dataB.query('VF > 0.4')
-
+def filter_2(data):
+    '''
+    Take the variants which VF > 0.4
+    '''
+    if "VF" in data.columns:
+        data["VF"] = data["VF"].astype(float)
+        return data.query('VF > 0.4')
+    else:
+        return data
 
    ### 4. Variants comparison
 
-# Two variants are the same if  CHROM, POS, REF, ALT and GT are equal
-
-dataA['CHROMPOSREFALT']= dataA["CHROM"].apply(str)+"."+dataA["POS"].apply(str)+dataA["REF"]+"."+dataA["ALT"]
-
-dataB['CHROMPOSREFALT']= dataB["CHROM"].apply(str)+"."+dataB["POS"].apply(str)+dataB["REF"]+"."+dataB["ALT"]
-
-
-df3 = dataA[['CHROMPOSREFALT', 'GT']].copy()
-df4 = dataB[['CHROMPOSREFALT', 'GT']].copy()
-frames = [df3,df4]
-semi_final_df = pd.concat(frames)
-# semi_final_df saves the CHROM, POS, REF, ALT in the first column and the GT in the second column
+def Unit_identifier(data):
+    '''
+    Two variants are the same if  CHROM, POS, REF, ALT and GT are equal
+    Here, the unit identifier is created
+    '''
+    return data["CHROM"].apply(str)+"."+data["POS"].apply(str)+data["REF"]+"."+data["ALT"]
 
 
-#Example:
-'''
-CHROMPOSREFALT     GT
+def concatenate_samples(data1,data2):
+    '''
+    To proceeded, both data frames need to be concatenate 
+    '''
+    frames = [data1[['CHROMPOSREFALT', 'GT']],data2[['CHROMPOSREFALT', 'GT']]]
+    return pd.concat(frames)
 
-chr1.123456T.C     0/1       From sample 1
-chr2.123456A.C     1/1       From sample 1
-chr1.123456T.C     0/1       From sample 2
+def match_variants(data):
+    '''
+    Match common variants in one column and put the GT values of both samples
+    in different columns.
+    No common variants are also set in the first column giving NaN values in the
+    third column. I have solved this substituting the NaN for 99/99 values
+    '''
+    matched_df = (data.assign(key=data.groupby('CHROMPOSREFALT').cumcount())
+        .pivot('CHROMPOSREFALT','key','GT')
+        .rename(columns=lambda x:f"Sample{x+1}")
+        .rename_axis(columns=None).reset_index())
+    return matched_df.replace(np.NaN,"99/99" )
 
-'''
+def count_hom_het_variants(data):
+    data['Genotype1'] = data.iloc[:,2].apply(lambda x: x.split('/' or '|')[0])
+    data['Genotype2'] = data.iloc[:,2].apply(lambda x: x.split('/' or '|')[1])
+    return np.select([data['Matches'] & data['Genotype1'].eq(data['Genotype2']),
+                       data['Matches'] & data['Genotype1'].ne(data['Genotype2'])],
+                       choicelist=["Hom", "Het"],
+                       default=pd.NA)
 
 
-# Let´s reduce the number of rows
-final_df=(semi_final_df.assign(key=semi_final_df.groupby('CHROMPOSREFALT').cumcount())
-      .pivot('CHROMPOSREFALT','key','GT')
-      .rename(columns=lambda x:f"Sample{x+1}")
-      .rename_axis(columns=None).reset_index())
+if __name__ == "__main__":
+    # execute only if run as a script
 
-# Final_df contains all common and non-common variants that passed the filters
+    # Enter the path/file names
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vcf1', type=str, required=True)
+    parser.add_argument('--vcf2', type=str, required=True)
+    args = parser.parse_args()
+    NAME_FILE_1 = args.vcf1
+    NAME_FILE_2 = args.vcf2
 
-# The pivot command gives NaNs in the column of the second sample
-# when a variant is only found in one of the samples
-# This NaN values return errors in the following lines.
-# This is an error I have found when the script was developed
-# To solve the issue without introducing many changes,
-# NaN values are replace for 99/99 values.
-final_df = final_df.replace(np.NaN,"99/99" )
+    # Load the files
+    dataA = load_sample(NAME_FILE_1)
+    dataB = load_sample(NAME_FILE_2)
 
-   ### 4. See how many GT match
+    # Save now the data of this sample and its name (we will need for the report)
+    dataA,Name_sample_1 = take_sample(dataA,NAME_FILE_1)
+    dataB,Name_sample_2 = take_sample(dataB,NAME_FILE_2)
 
-# Variables needed for both type of reports
+    # Filter 1
+    dataA = filter_1(dataA)
+    dataB = filter_1(dataB)
 
-r0 = os.path.basename(NAME_FILE_1) # Get the  file name of a path/file_name
-r1 = Name_sample1                 # Get the name of the sample 1
-r2 = os.path.basename(NAME_FILE_2)
-r3 = Name_sample2
+    # Filter 2
+    dataA = filter_2(dataA)
+    dataB = filter_2(dataB)
+    print(dataB)
+    # Unit_identifier
+    dataA['CHROMPOSREFALT'] = Unit_identifier(dataA)
+    dataB['CHROMPOSREFALT'] = Unit_identifier(dataB)
 
-if "Sample2" not in final_df.columns:
+    # Concatenate both data frames
+    semi_final_df = concatenate_samples(dataA,dataB)
+
+    # Match common variants
+    final_df= match_variants(semi_final_df)
+
+#### Generate report ####
+
+    # Variables needed for both type of reports
+
+    r0 = os.path.basename(NAME_FILE_1) # Get the  file name of a path/file_name
+    r1 = Name_sample_1                 # Get the name of the sample 1
+    r2 = os.path.basename(NAME_FILE_2)
+    r3 = Name_sample_2
+
+    if "Sample2" not in final_df.columns:
     # If Sample2 is not in final_df, that means there is not common position between the samples.
-    REPORT_A = '''
+    # This is a the most simple kind of report with only the name of the files
+    # and samples and informing that no common positions have been found between the samples
+        REPORT_A = '''
  _____________________________  REPORT  ________________________________________ 
 
-vcf 1: {0}  AND its sample name: {1}  
-vcf 2:  {2}  AND its sample name: {3}
+   vcf 1: {0}  AND its sample name: {1}  
+   vcf 2:  {2}  AND its sample name: {3}
 
    No common positions found between samples
 
  ____________________________ END REPORT  _______________________________________
 '''
-    print(REPORT_A.format(r0,r1,r2,r3,))
+        print(REPORT_A.format(r0,r1,r2,r3,))
 
-else:
-    # else match common position and carry on the report.
-    final_df["Matches"] = np.where(final_df["Sample1"] == final_df["Sample2"], True, False)
-    final_df.columns = ['CHROMPOSREFALT',os.path.basename(NAME_FILE_1),os.path.basename(NAME_FILE_2),"Matches" ]
+    # If Sample2 is in final_df.columns, that means that at least one common variant has been found
+    # so, generate the data from these common variants
+    else:
+    # Else count common positions and carry on the report.
+        final_df["Matches"] = np.where(final_df["Sample1"] == final_df["Sample2"], True, False)
+        final_df.columns = ['CHROMPOSREFALT',os.path.basename(NAME_FILE_1),os.path.basename(NAME_FILE_2),"Matches" ]
 
-    # 6.  To get hom and het
+    #  Get and count hom and het common variants
+        final_df['Hom/het'] = count_hom_het_variants(final_df)
 
-    final_df['Genotype1'] = final_df.iloc[:,2].apply(lambda x: x.split('/' or '|')[0])
-    final_df['Genotype2'] = final_df.iloc[:,2].apply(lambda x: x.split('/' or '|')[1])
+    # Generate the second type of report
 
-    final_df['Hom/het'] = np.select([final_df['Matches'] & final_df['Genotype1'].eq(final_df['Genotype2']),
-                       final_df['Matches'] & final_df['Genotype1'].ne(final_df['Genotype2'])],
-                       choicelist=["Hom", "Het"],
-                       default=pd.NA)
-
-   ### 6. Generate results in the terminal
-
-# Variable to introduce in the report :
-
-    r4 = final_df['Matches'].value_counts().get(True, 0) # Count trues if any, return 0
-    r5 = final_df['Hom/het'].value_counts().get("Hom", 0)
-    r6 =final_df['Hom/het'].value_counts().get("Het", 0)
-    r7 = final_df['Matches'].value_counts().get(False, 0)
-    r8 = len(final_df)
-    r9 = r4/(r4+r7)
-    REPORT_B = '''
+        r4 = final_df['Matches'].value_counts().get(True, 0) # Count trues if any, return 0
+        r5 = final_df['Hom/het'].value_counts().get("Hom", 0)
+        r6 =final_df['Hom/het'].value_counts().get("Het", 0)
+        r7 = final_df['Matches'].value_counts().get(False, 0)
+        r8 = len(final_df)
+        r9 = r4/(r4+r7)
+        REPORT_B = '''
  _____________________________  REPORT  ________________________________________ 
 
 vcf 1: {0}  AND its sample name: {1}  
@@ -225,4 +214,5 @@ Percentage in common: {4}/{8}= {9}
 
  ____________________________ END REPORT  _______________________________________
 '''
-    print(REPORT_B.format(r0,r1,r2,r3,r4,r5,r6,r7,r8,r9))
+        print(REPORT_B.format(r0,r1,r2,r3,r4,r5,r6,r7,r8,r9))
+
